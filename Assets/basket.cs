b7ced61ce08778b6fd34416da71273ceb2145c40
Hredoyyy <rakibulhassan5700@gmail.com>
2023-12-02 17:08:24 +0600

    basket added and control added

---
+++ Assets/basket.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class basket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey("a")){
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-5,0);            
+        }
+        if (Input.GetKey("d")){
+            GetComponent<Rigidbody2D>().velocity = new Vector2(5,0);            
+        }
+        if (!Input.GetKey("a") && !Input.GetKey("d")){
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);            
+        }
+        
+    }
+}
